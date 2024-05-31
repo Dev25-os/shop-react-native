fbce7d86f791570ef575206dd0f6f0dd84f35141
Dev25-os <bikeshgupta50041@gmail.com>
2024-05-31 12:11:27 +0530

    added auth

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,8 +1,27 @@
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import LoginScreen from "../components/LoginScreen"
+import * as SecureStore from "expo-secure-store";
+
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 
 export default function RootLayout() {
-
   useFonts({
     'poppins-regular': require("../assets/fonts/Poppins-Regular.ttf"),
     'poppins-medium': require("../assets/fonts/Poppins-Medium.ttf"),
@@ -11,8 +30,16 @@ export default function RootLayout() {
 
 
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(tabs)" />
-    </Stack>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+      <SignedIn>
+
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </SignedIn>
+      <SignedOut>
+        <LoginScreen />
+      </SignedOut>
+    </ClerkProvider>
   );
 }
--- constants/Colors.js
+++ constants/Colors.js
@@ -23,5 +23,7 @@ export const Colors = {
     tabIconDefault: "#9BA1A6",
     tabIconSelected: tintColorDark,
   },
-  PRIMARY: "#111111",
+  PRIMARY: "#615EFC",
+  SECONDARY: "#373A40",
+
 };
